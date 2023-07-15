9decb8e5347868ccf146011a73ef115c7867482a
Shehzad Aslam Oza <32712842+ShehzadAslamOza@users.noreply.github.com>
2023-07-15 18:45:57 +0500

    Fixed issue with images on cards in mobile devices

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -4,34 +4,34 @@ import { useState } from "react";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
-import { Processor } from "postcss";
 
-const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
-  const [copied, setCopied] = useState("");
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const { data: session } = useSession();
   const pathName = usePathname();
   const router = useRouter();
 
+  const [copied, setCopied] = useState("");
+
+  const handleProfileClick = () => {
+    console.log(post);
+
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
-    setTimeout(() => setCopied(""), 3000);
-  };
-
-  const handleProfileClick = () => {
-    if (post.creator.email === session?.user.email) {
-      router.push("/profile");
-    } else {
-      router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
-    }
+    setTimeout(() => setCopied(false), 3000);
   };
 
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div
-          onClick={handleProfileClick}
           className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
         >
           <Image
             src={post.creator.image}
@@ -40,27 +40,25 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             height={40}
             className="rounded-full object-contain"
           />
+
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
+          </div>
         </div>
 
-        <div
-          className="flex flex-col cursor-pointer"
-          onClick={handleProfileClick}
-        >
-          <h3 className="font-satoshi font-semibold text-gray-900">
-            {post.creator.username}
-          </h3>
-          <p className="font-inter text-sm text-gray-500">
-            {post.creator.email}
-          </p>
-        </div>
-        <div className="copy_btn">
+        <div className="copy_btn" onClick={handleCopy}>
           <Image
-            onClick={handleCopy}
             src={
               copied === post.prompt
                 ? "/assets/icons/tick.svg"
                 : "/assets/icons/copy.svg"
             }
+            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
             width={12}
             height={12}
           />
@@ -72,7 +70,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
 
       {session?.user.id === post.creator._id && pathName === "/profile" && (
